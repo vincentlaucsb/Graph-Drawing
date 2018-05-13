e83037619f8bde5f3f3f0a4272065712e947280f
vincentlaucsb <vincela9@gmail.com>
2018-05-13 16:27:20 -0700

    Added tree drawing via linear program

--- README.md
+++ README.md
@@ -8,11 +8,11 @@ This algorithm visualizes nodes as rings hooked onto steel springs (edges). In a
 vertices, so that they don't completely overlap.
 
 ### Algorithm Trace of Eades' Algorithm for K9
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/k9.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/k9.svg?sanitize=true" />
 
 ### Other Complete Graphs
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/k4.svg?sanitize=true" />
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/k5.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/k4.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/k5.svg?sanitize=true" />
 
 ## Tuttes' Barycenter Algorithm
 In this classic force directed algorithm, a strictly convex polygon is drawn on the exterior, with some nodes nailed down onto the
@@ -21,20 +21,20 @@ This is an algorithm of limited utility, since it doesn't handle many nodes, and
 results for 3-connected graphs.
 
 ### Hypercube
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/hypercube.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/hypercube.svg?sanitize=true" />
 
 ### Petersen
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/petersen.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/petersen.svg?sanitize=true" />
 
 ### Prisms
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/prism_5.svg?sanitize=true" />
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/prism_8.svg?sanitize=true" />
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/prism_10.svg?sanitize=true" />
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/prism_15.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/prism_5.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/prism_8.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/prism_10.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/prism_15.svg?sanitize=true" />
 
 ### Angular Resolution
 One of the drawbacks of this algorithm is that the angular resolution becomes poorer as the number of vertices increases. This 
 can be seen in the prisms on 20 and 40 vertices respectively shown below.
 
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/prism_20.svg?sanitize=true" />
-<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/prism_40.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/prism_20.svg?sanitize=true" />
+<img width="500px" src="https://raw.githubusercontent.com/vincentlaucsb/Force-Directed-Drawing/master/animations/prism_40.svg?sanitize=true" />
---
+++ src/tree_lp.cpp
@@ -0,0 +1,184 @@
+/* spxsamp1.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <algorithm>
+#include <unordered_map>
+#include <deque>
+#include "glpk.h"
+
+struct TreeNode {
+    int id;
+    TreeNode* left;
+    TreeNode* right;
+};
+
+int main()
+{
+    const int num_nodes = 7;
+    int left_sons = 3, right_sons = 3;
+    int num_constraints = num_nodes * 2 // Width constraint
+        + left_sons + right_sons  // 2nd constraint
+        + std::min(left_sons, right_sons); // Equal separation
+
+    glp_prob *P;
+    P = glp_create_prob();
+
+    // Variable for each node + X, x
+    glp_add_cols(P, num_nodes + 2);
+    for (int i = 1; i <= num_nodes + 2; i++) glp_set_col_bnds(P, i, GLP_FR, NULL, NULL); // Make columns unbounded
+    glp_add_rows(P, num_constraints);
+
+    // Set objective function
+    glp_set_obj_dir(P, GLP_MIN);
+    glp_set_obj_coef(P, 1, 1);  // X
+    glp_set_obj_coef(P, 2, -1); // -x
+
+    // Complete binary tree of height 2
+    TreeNode root;
+    root.left = new TreeNode();
+    root.right = new TreeNode();
+    
+    root.left->left = new TreeNode();
+    root.left->right = new TreeNode();
+
+    root.right->left = new TreeNode();
+    root.right->right = new TreeNode();
+
+    // Keep track of all nodes on a given level
+    std::unordered_map<int, std::vector<TreeNode*>> levels;
+
+    // Assign IDs
+    // First item of pair = parent of node, second item = node
+    using NodeInfo = std::pair<TreeNode*, TreeNode*>;
+    std::deque<NodeInfo> children = { std::make_pair(nullptr, &root) };
+    int current_id = 3, current_row = 1, current_level = 0; // Can't assign to 0; 1 = X, 2 = x
+
+    while (!children.empty()) {
+        NodeInfo current = children.front();
+        auto& current_node = current.second;
+
+        if ((current_level > 0)) {
+            if (std::find(levels[current_level - 1].begin(), levels[current_level - 1].end(), current.first) == levels[current_level - 1].end()) {
+                current_level++;
+            }
+        }
+
+        current_node->id = current_id;
+        if (current_node->left) children.push_back( std::make_pair(current_node, current_node->left) );
+        if (current_node->right) children.push_back( std::make_pair(current_node, current_node->right) );
+
+        // Add to mapping of levels to nodes
+        levels[current_level].push_back(current_node);
+
+        // Also add width constraints: indices refer to X, current_id
+        const int ind_up[] = { NULL, 1, current_id },
+            ind_down[] = { NULL, 2, current_id };
+        const double val[] = { NULL, -1, 1 };
+        glp_set_mat_row(P,
+            current_row, // Index of constraint
+            2,           // Number of values
+            &(ind_up[0]),   // Indices of values
+            &(val[0])    // Values
+        );
+        current_row++;
+        glp_set_row_bnds(P, current_row, GLP_LO, 0, NULL);
+
+        glp_set_mat_row(P,
+            current_row, // Index of constraint
+            2,           // Number of values
+            &(ind_down[0]),   // Indices of values
+            &(val[0])    // Values
+        );
+        current_row++;
+        glp_set_row_bnds(P, current_row, GLP_UP, NULL, 0);
+
+        children.pop_front();
+        current_id++;
+
+        if (current_level == 0) current_level++;
+    }
+
+    // Calculate 2nd constraint + equal separation constraints
+    for (auto& l : levels) {
+        for (auto& node : l.second) {
+            if (node->left) {
+                const int ind[] = { NULL, node->id, node->left->id };
+                const double val[] = { NULL, 1, -1 };
+
+                glp_set_mat_row(P,
+                    current_row, // Index of constraint
+                    2,           // Number of values
+                    &(ind[0]),   // Indices of values
+                    &(val[0])    // Values
+                );
+                glp_set_row_bnds(P, current_row, GLP_LO, 1, 0);
+                current_row++;
+            }
+
+            if (node->right) {
+                const int ind[] = { NULL, node->id, node->right->id };
+                const double val[] = { NULL, -1, 1 };
+
+                glp_set_mat_row(P,
+                    current_row, // Index of constraint
+                    2,           // Number of values
+                    &(ind[0]),   // Indices of values
+                    &(val[0])    // Values
+                );
+                glp_set_row_bnds(P, current_row, GLP_LO, 1, 0);
+                current_row++;
+            }
+
+            // Equal separation
+            if (node->left && node->right) {
+                const int ind[] = { NULL, node->id, node->left->id, node->right->id };
+                const double val[] = { NULL, -2, 1, 1 };
+
+                glp_set_mat_row(P,
+                    current_row, // Index of constraint
+                    3,           // Number of values
+                    &(ind[0]),   // Indices of values
+                    &(val[0])    // Values
+                );
+                glp_set_row_bnds(P, current_row, GLP_FX, 0, 0);
+                current_row++;
+            }
+        }
+    }
+
+    // Calculate separation constraints
+    for (auto& l : levels) {
+        // Add correct number of rows
+        if (l.second.size() - 1) {
+            glp_add_rows(P, l.second.size() - 1); // For each adjacent pair of nodes
+            int first_id = l.second.front()->id, second_id = l.second.back()->id;
+            for (int i = first_id; i < second_id; i++) {
+                const int ind[] = { NULL, i, i + 1 };
+                const double val[] = { NULL, -1, 1 };
+
+                glp_set_mat_row(P,
+                    current_row, // Index of constraint
+                    2,           // Number of values
+                    &(ind[0]),   // Indices of values
+                    &(val[0])    // Values
+                );
+                glp_set_row_bnds(P, current_row, GLP_LO, 2, NULL);
+
+                current_row++;
+            }
+        }
+    }
+
+    // Write model (debugging)
+    glp_write_lp(P, NULL, "model.txt");
+
+    // Solve problem
+    glp_simplex(P, NULL);
+    glp_print_sol(P, "test.txt");
+
+    glp_delete_prob(P);
+    return 0;
+}
+
+/* eof */

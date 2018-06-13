faa9c112f9ca3521737506cccda9dda20319464f
vincentlaucsb <vincela9@gmail.com>
2018-06-13 14:31:47 -0700

    Added ability to load graphs via CSV

--- src/animate_spring.cpp
+++ src/animate_spring.cpp
@@ -6,23 +6,32 @@ int main(int argc, char** argv) {
     using namespace csv;
     using namespace force_directed;
 
-    cxxopts::Options options(argv[0], "Animates drawing a complete graph on n vertices");
-    options.positional_help("[output file] [n]");
+    cxxopts::Options options(argv[0], "Animates drawing a complete graph");
+    options.positional_help("[output file]");
     options.add_options("required")
-        ("f,file", "Output file", cxxopts::value<std::string>())
-        ("n,vertices", "Animate a complete graph on m vertices", cxxopts::value<int>());
+        ("file", "Output file", cxxopts::value<std::string>());
     options.add_options("optional")
+        ("n,vertices", "Specify the number of vertices for the complete graph",
+            cxxopts::value<int>()->default_value("8"))
         ("c,cube", "Draw the regular cube")
-        ("k,tesseract", "Draw the 4-cube")
+        ("d,tesseract", "Draw the tesseract")
         ("t,three_reg", "Draw a three-regular graph")
         ("s,still", "Draw a still drawing of the graph")
         ("r,trace", "Create an algorithm trace of the spring layout")
+        ("g,graph", "Read a CSV file containing edge pairs",
+            cxxopts::value<std::string>()->default_value(""))
         ("p,pos", "Read a CSV file containing positions for vertices",
-            cxxopts::value<std::string>()->default_value(""));
+            cxxopts::value<std::string>()->default_value(""))
+        ("luv", "Specify the parameters of the spring system",
+            cxxopts::value<double>()->default_value("400"))
+        ("kuv1", "Specify the parameters of the spring system",
+            cxxopts::value<double>()->default_value("2"))
+        ("kuv2", "Specify the strength of the electrical force between vertices",
+            cxxopts::value<double>()->default_value("1"));
 
-    options.parse_positional({ "file", "vertices" });
+    options.parse_positional({ "file" });
 
-    if (argc < 3) {
+    if (argc < 2) {
         std::cout << options.help({ "required", "optional" }) << std::endl;
         return 1;
     }
@@ -30,20 +39,49 @@ int main(int argc, char** argv) {
     auto result = options.parse(argc, (const char**&)argv);
     
     std::string file = result["file"].as<std::string>(),
+        graph_file = result["graph"].as<std::string>(),
         pos_file = result["pos"].as<std::string>();
+
     bool still = result["still"].as<bool>(),
         side_by_side = result["trace"].as<bool>(),
         cube = result["cube"].as<bool>(),
         tesseract = result["tesseract"].as<bool>(),
         three_reg = result["three_reg"].as<bool>();
+
     int n = result["vertices"].as<int>();
-    ForceDirectedParams params = { 400, 2, 1 };
+
+    ForceDirectedParams params = {
+        result["luv"].as<double>(), // 400
+        result["kuv1"].as<double>(), // 2
+        result["kuv2"].as<double>(), // 1
+    };
 
     try {
         auto graph_ptr = TSnap::GenFull<PUNGraph>(n);
         auto graph = *graph_ptr;
-        VertexPos pos = random_layout(graph);
 
+        if (!graph_file.empty()) {
+            CSVReader reader(graph_file);
+            std::vector<CSVField> row;
+            std::set<int> nodes;
+            std::vector<std::pair<int, int>> edges;
+
+            while (reader.read_row(row)) {
+                int first = row[0].get_int(),
+                    second = row[1].get_int();
+
+                nodes.insert(first);
+                nodes.insert(second);
+
+                edges.push_back(std::make_pair(first, second));
+            }
+
+            graph = TUNGraph();
+            for (auto& u : nodes) graph.AddNode(u);
+            for (auto& pair : edges) graph.AddEdge(pair.first, pair.second);
+        }
+
+        VertexPos pos = random_layout(graph);
         if (!pos_file.empty()) {
             CSVReader reader(pos_file);
             std::vector<CSVField> row;

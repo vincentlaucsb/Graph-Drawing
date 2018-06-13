b401a80994e9386fd063a77db1dbba08c6e4882f
vincentlaucsb <vincela9@gmail.com>
2018-06-12 23:22:53 -0700

    Added hypercube drawings

--- src/animate_spring.cpp
+++ src/animate_spring.cpp
@@ -1,7 +1,9 @@
+#include "csv_parser.h"
 #include "force_directed.h"
 #include "cxxopts.hpp"
 
 int main(int argc, char** argv) {
+    using namespace csv;
     using namespace force_directed;
 
     cxxopts::Options options(argv[0], "Animates drawing a complete graph on n vertices");
@@ -13,8 +15,10 @@ int main(int argc, char** argv) {
         ("c,cube", "Draw the regular cube")
         ("k,tesseract", "Draw the 4-cube")
         ("t,three_reg", "Draw a three-regular graph")
-        ("s,side", "Create an algorithm trace of the spring layout",
-            cxxopts::value<bool>()->default_value("false"));
+        ("s,still", "Draw a still drawing of the graph")
+        ("r,trace", "Create an algorithm trace of the spring layout")
+        ("p,pos", "Read a CSV file containing positions for vertices",
+            cxxopts::value<std::string>()->default_value(""));
 
     options.parse_positional({ "file", "vertices" });
 
@@ -25,8 +29,10 @@ int main(int argc, char** argv) {
     
     auto result = options.parse(argc, (const char**&)argv);
     
-    std::string file = result["file"].as<std::string>();
-    bool side_by_side = result["side"].as<bool>(),
+    std::string file = result["file"].as<std::string>(),
+        pos_file = result["pos"].as<std::string>();
+    bool still = result["still"].as<bool>(),
+        side_by_side = result["trace"].as<bool>(),
         cube = result["cube"].as<bool>(),
         tesseract = result["tesseract"].as<bool>(),
         three_reg = result["three_reg"].as<bool>();
@@ -38,41 +44,27 @@ int main(int argc, char** argv) {
         auto graph = *graph_ptr;
         VertexPos pos = random_layout(graph);
 
-        if (cube) {
-            graph = hypercube();
-            pos = {
-                { 4,{ 0, 0 } },
-                { 5,{ 200, 0 } },
-                { 6,{ 180, -200 } },
-                { 7,{ 0, -200 } },
-                { 0,{ 100, 100 } },
-                { 1,{ 300, 100 } },
-                { 2,{ 300, -80 } },
-                { 3,{ 100, -100 } }
-            };
-        }
-        if (tesseract) {
-            graph = hypercube_4();
-            pos = {
-                { 0,{ 0, 0 } },
-                { 1,{ 200, 0 } },
-                { 2,{ 200, -200 } },
-                { 3,{ 0, -200 } },
-                { 4,{ 100, 100 } },
-                { 5,{ 300, 100 } },
-                { 6,{ 300, -100 } },
-                { 7,{ 100, -100 } }, // 
-                { 8,{ 0 + 800, 0 + 100 } },
-                { 9,{ 200 + 800, 0 + 100 } },
-                { 10,{ 200 + 800, -200 + 100 } },
-                { 11,{ 0 + 800, -200 + 100 } },
-                { 12,{ 100 + 800, 100 + 100 } },
-                { 13,{ 300 + 800, 100 + 100 } },
-                { 14,{ 300 + 800, -100 + 100 } },
-                { 15,{ 100 + 800, -100 + 100 } }
-                };
+        if (!pos_file.empty()) {
+            CSVReader reader(pos_file);
+            std::vector<CSVField> row;
+
+            while (reader.read_row(row)) {
+                pos[row[0].get_int()] = std::make_pair(
+                    row[1].get_float(), row[2].get_float());
+            }
+
+            if (pos.size() < graph.GetNodes()) {
+                throw std::runtime_error("Position file has " +
+                    std::to_string(pos.size()) + " lines "
+                    "while graph has " + std::to_string(graph.GetNodes())
+                    + " nodes.");
+            }
         }
 
+        if (cube) {
+            graph = hypercube();
+        }
+        if (tesseract) graph = hypercube_4();
         if (three_reg) {
             graph = three_reg_6();
             auto points = SVG::util::polar_points(graph.GetNodes(), 0, 0, 100);
@@ -95,12 +87,18 @@ int main(int argc, char** argv) {
             }
 
             auto final_svg = SVG::merge(frames, 1000, 250);
-            std::ofstream graph_out(file + ".svg");
+            std::ofstream graph_out(file);
+            graph_out << std::string(final_svg);
+        }
+        else if (still) {
+            auto& final_svg = frames.back();
+            final_svg.autoscale();
+            std::ofstream graph_out(file);
             graph_out << std::string(final_svg);
         }
         else {
             auto final_svg = SVG::frame_animate(frames, 5);
-            std::ofstream graph_out(file + ".svg");
+            std::ofstream graph_out(file);
             graph_out << std::string(final_svg);
         }
     }

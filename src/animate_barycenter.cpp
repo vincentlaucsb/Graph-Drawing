2dfa77e3a63ce92f584c50d0031060a0647019df
vincentlaucsb <vincela9@gmail.com>
2018-06-14 23:02:49 -0700

    Added more figures blah blah

--- src/animate_barycenter.cpp
+++ src/animate_barycenter.cpp
@@ -1,6 +1,49 @@
 #include "force_directed.h"
 #include "cxxopts.hpp"
+#include <iomanip> // setprecision
 #include <sstream>
+
+std::string latex(const Eigen::MatrixXd&);
+std::string latex(const char name, const Eigen::VectorXd&);
+
+std::string latex(const Eigen::MatrixXd& m) {
+    // Print the LaTeX code for a matrix
+    std::stringstream ret;
+    ret << std::setprecision(3) << "\\begin{bmatrix}" << std::endl;
+
+    for (int i = 0; i < m.rows(); i++) {
+        ret << "\t";
+        for (int j = 0; j < m.cols(); j++) {
+            ret << m(i, j);
+            if (j + 1 < m.cols()) ret << " & ";
+            else ret << " \\\\ " << std::endl;
+        }
+    }
+
+    ret << "\\end{bmatrix}" << std::endl;
+
+    return ret.str();
+}
+
+std::string latex(const char name, const Eigen::VectorXd& v) {
+    // Print the LaTeX code for a vector
+    std::stringstream r1, r2;
+    r1 << std::setprecision(3) << "\\begin{bmatrix}" << std::endl;
+    r2 << std::setprecision(3) << "\\begin{bmatrix}" << std::endl;
+
+    for (int i = 0; i < v.rows(); i++) {
+        r1 << "\t" << name << "_" << i + 1;
+        r2 << "\t" << v(i);
+
+        r1 << " \\\\ " << std::endl;
+        r2 << " \\\\ " << std::endl;
+    }
+
+    r1 << "\\end{bmatrix}" << std::endl;
+    r2 << "\\end{bmatrix}" << std::endl;
+
+    return r1.str() + "=" + r2.str();
+}
 
 int main(int argc, char** argv) {
     using namespace force_directed;
@@ -15,10 +58,9 @@ int main(int argc, char** argv) {
     options.add_options("optional")
         ("h,hypercube", "Animate drawing a hypercube of order 3")
         ("p,petersen", "Animate drawing the Petersen graph", cxxopts::value<bool>()->default_value("false"))
+        ("r,prism", "Animate drawing the Prism graph on n vertices", cxxopts::value<int>()->default_value("0"))
         ("g,generalized", "Animate drawing the Generalized Petersen graph GP(n, k)",
             cxxopts::value<std::string>()->default_value(""))
-        ("r,prism", "Animate drawing the Prism graph on n vertices", cxxopts::value<int>()->default_value("0"))
-        ("k,complete", "Draw the complete graph on n vertices", cxxopts::value<int>()->default_value("0"))
         ("w,width", "Specify the width of the drawing", cxxopts::value<int>()->default_value("500"))
         ("s,static", "Draw a still image of the graph (uses linear solver)", cxxopts::value<bool>()->default_value("false"));
 
@@ -78,19 +120,19 @@ int main(int argc, char** argv) {
 
         graph_out << std::string(output.image);
         std::cout << "Matrix" << std::endl;
-        std::cout << output.matrix << std::endl << std::endl;
+        std::cout << latex(output.matrix) << std::endl;
 
         std::cout << "Fixed vertex positions (x)" << std::endl;
-        std::cout << output.fixed_x << std::endl << std::endl;
+        std::cout << latex('x', output.fixed_x) << std::endl;
 
         std::cout << "Fixed vertex positions (y)" << std::endl;
-        std::cout << output.fixed_y << std::endl << std::endl;
+        std::cout << latex('y', output.fixed_y) << std::endl;
 
         std::cout << "Free vertex positions (x)" << std::endl;
-        std::cout << output.sol_x << std::endl << std::endl;
+        std::cout << latex('x', output.sol_x) << std::endl;
 
         std::cout << "Free vertex positions (y)" << std::endl;
-        std::cout << output.sol_y << std::endl;
+        std::cout << latex('y', output.sol_y) << std::endl;
     }
     else {
         std::vector<SVG::SVG> frames = barycenter_layout(graph, vertices, width);
